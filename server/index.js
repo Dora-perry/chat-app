706bdd97b3a10e1e006fdb59b0819ddae3060aad
Decagon <theodorachinonso@gmail.com>
2023-02-22 13:44:13 +0100

    files changes

--- server/index.js
+++ server/index.js
@@ -7,14 +7,7 @@ const app = express();
 const socket = require("socket.io");
 require("dotenv").config();
 
-app.use(
-  cors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 mongoose
