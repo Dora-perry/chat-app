5d61d16fada8324b8f2c4fea08c5da8e0d2f25c8
Decagon <theodorachinonso@gmail.com>
2023-02-22 10:00:49 +0100

    cors handle

--- server/index.js
+++ server/index.js
@@ -7,7 +7,14 @@ const app = express();
 const socket = require("socket.io");
 require("dotenv").config();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 app.use(express.json());
 
 mongoose
